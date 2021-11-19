a14e1e9adc7c4430c2eda556e4e15c36004a8d66
ldg0315 <ldg315@naver.com>
2021-11-19 13:17:55 +0900

    Add files via upload
    버튼 함수 수정

--- lib/screen/main/main_screen.dart
+++ lib/screen/main/main_screen.dart
@@ -15,24 +15,15 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  late int zero;
-  late int zero2;
-  late int one;
-  late int one2;
-  late int two;
-  late int two2;
-  late int button;
+  late int n;
+  late int m;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    zero = 0;
-    zero2 = 0;
-    one = 1;
-    one2 = 1;
-    two = 2;
-    two2 = 2;
+    n = 0;
+    m = 0;
   }
 
   @override
@@ -81,7 +72,7 @@ class _MainScreenState extends State<MainScreen> {
                                 SizedBox(
                                     width: 415,
                                     height: 314,
-                                    child: ContentsPart(mainSales: mainSaleList[zero]),
+                                    child: ContentsPart(mainSales: mainSaleList[n%mainSaleList.length]),
                                 ),
                                 const SizedBox(
                                   height: 30,
@@ -89,7 +80,7 @@ class _MainScreenState extends State<MainScreen> {
                                 SizedBox(
                                   width: 415,
                                   height: 72,
-                                  child: SubPart(mainSales: mainSaleList[one]),
+                                  child: SubPart(mainSales: mainSaleList[(n+1)%mainSaleList.length]),
                                 ),
                                 const SizedBox(
                                   height: 38,
@@ -97,7 +88,7 @@ class _MainScreenState extends State<MainScreen> {
                                 SizedBox(
                                   width: 415,
                                   height: 72,
-                                  child: SubPart(mainSales: mainSaleList[two]),
+                                  child: SubPart(mainSales: mainSaleList[(n+2)%mainSaleList.length]),
                                 ),
                                 const SizedBox(
                                   height: 30,
@@ -109,16 +100,7 @@ class _MainScreenState extends State<MainScreen> {
                                       child: IconButton(
                                           onPressed: () {
                                             setState(() {
-                                              zero++;
-                                              one++;
-                                              two++;
-                                              if (two == mainSaleList.length) {
-                                                two = 0;
-                                              } else if (one == mainSaleList.length) {
-                                                one = 0;
-                                              } else if (zero == mainSaleList.length) {
-                                                zero = 0;
-                                              }
+                                              n++;
                                             });
                                           },
                                           icon: const Icon(Icons.arrow_drop_up, size: 50, color: Colors.grey,)
@@ -128,16 +110,7 @@ class _MainScreenState extends State<MainScreen> {
                                       child: IconButton(
                                           onPressed: () {
                                             setState(() {
-                                              zero--;
-                                              one--;
-                                              two--;
-                                              if (two < 0) {
-                                                two = mainSaleList.length - 1;
-                                              } else if (one < 0) {
-                                                one = mainSaleList.length - 1;
-                                              } else if (zero < 0) {
-                                                zero = mainSaleList.length - 1;
-                                              }
+                                              n--;
                                             });
                                           },
                                           icon: const Icon(Icons.arrow_drop_down, size: 50, color: Colors.grey,)
@@ -182,7 +155,7 @@ class _MainScreenState extends State<MainScreen> {
                                 SizedBox(
                                     width: 415,
                                     height: 314,
-                                    child: ContentsPart2(mainPurchase: mainPurchaseList[zero2],)
+                                    child: ContentsPart2(mainPurchase: mainPurchaseList[m%mainPurchaseList.length],)
                                 ),
                                 const SizedBox(
                                   height: 30,
@@ -190,7 +163,7 @@ class _MainScreenState extends State<MainScreen> {
                                 SizedBox(
                                   width: 415,
                                   height: 72,
-                                  child: SubPart2(mainPurchase: mainPurchaseList[one2],)
+                                  child: SubPart2(mainPurchase: mainPurchaseList[(m+1)%mainPurchaseList.length],)
                                 ),
                                 const SizedBox(
                                   height: 38,
@@ -198,9 +171,9 @@ class _MainScreenState extends State<MainScreen> {
                                 SizedBox(
                                   width: 415,
                                   height: 72,
-                                  child: SubPart2(mainPurchase: mainPurchaseList[two2],),
+                                  child: SubPart2(mainPurchase: mainPurchaseList[(m+2)%mainPurchaseList.length],),
                                 ),
-                                SizedBox(
+                                const SizedBox(
                                   height: 30,
                                 ),
                                 Row(
@@ -210,16 +183,7 @@ class _MainScreenState extends State<MainScreen> {
                                       child: IconButton(
                                           onPressed: () {
                                             setState(() {
-                                              zero2++;
-                                              one2++;
-                                              two2++;
-                                              if (two2 == mainSaleList.length) {
-                                                two2 = 0;
-                                              } else if (one2 == mainSaleList.length) {
-                                                one2 = 0;
-                                              } else if (zero2 == mainSaleList.length) {
-                                                zero2 = 0;
-                                              }
+                                              m++;
                                             });
                                           },
                                           icon: const Icon(Icons.arrow_drop_up, size: 50, color: Colors.grey,)
@@ -229,16 +193,7 @@ class _MainScreenState extends State<MainScreen> {
                                       child: IconButton(
                                           onPressed: () {
                                             setState(() {
-                                              zero2--;
-                                              one2--;
-                                              two2--;
-                                              if (two2 < 0) {
-                                                two2 = mainSaleList.length - 1;
-                                              } else if (one2 < 0) {
-                                                one2 = mainSaleList.length - 1;
-                                              } else if (zero2 < 0) {
-                                                zero2 = mainSaleList.length - 1;
-                                              }
+                                              m--;
                                             });
                                           },
                                           icon: const Icon(Icons.arrow_drop_down, size: 50, color: Colors.grey,)
